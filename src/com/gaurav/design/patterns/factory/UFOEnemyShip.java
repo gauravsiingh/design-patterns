001b79822ec73f9347beac245dfe2ec4dc6b1d80
gauravsiingh <kgaurav9singh@gmail.com>
2019-05-23 10:08:00 +0530

    observer, factory and abstract factory patterns

---
+++ src/com/gaurav/design/patterns/factory/UFOEnemyShip.java
@@ -0,0 +1,13 @@
+package com.gaurav.design.patterns.factory;
+
+public class UFOEnemyShip extends EnemyShip {
+	
+	public UFOEnemyShip(){
+		
+		setName("UFO Enemy Ship");
+		
+		setDamage(20.0);
+		
+	}
+	
+}
